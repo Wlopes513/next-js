d396c2e8d590e236f8f1f173c3d2352e9934c880
Wlopes513 <wlopes_junior2013@hotmail.com>
2022-09-12 15:33:48 -0300

    Implemented buttons actions

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -1,4 +1,5 @@
 import Client from "../core/client"
+import { IconEdit, IconTrash } from "./icons";
 
 interface TableProps {
     clients: Client[]
@@ -14,14 +15,23 @@ export default function Table(props: TableProps) {
                     <th className="text-left p-4">Código</th>
                     <th className="text-left p-4">Nome</th>
                     <th className="text-left p-4">Idade</th>
+                    <th className="text-center p-4">Ações</th>
                 </tr>
             </thead>
             <tbody>
                 {clients?.map((client, i) => (
-                    <tr key={i} className={i % 2 === 0  ? 'bg-purple-200' : 'bg-purple-100'}>
+                    <tr key={i} className={i % 2 === 0 ? 'bg-purple-200' : 'bg-purple-100'}>
                         <td className="text-left p-4">{client.id}</td>
                         <td className="text-left p-4">{client.name}</td>
                         <td className="text-left p-4">{client.age}</td>
+                        <td className="text-left p-4 flex justify-center">
+                            <button type="button" className="flex justify-center items-center text-green-600 rounded-full hover:bg-purple-50 p-2 m-1">
+                                {IconEdit}
+                            </button>
+                            <button type="button" className="flex justify-center items-center text-red-500 rounded-full hover:bg-purple-50 p-2 m-1">
+                                {IconTrash}
+                            </button>
+                        </td>
                     </tr>
                 ))}
             </tbody>
